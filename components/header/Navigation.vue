b3273e55cfb57ca8f583b02e1acc5ed52dd7ef9a
Andy Terry <andyterrydesign@gmail.com>
2024-03-27 09:56:10 +0000

    Update Navigation.vue

--- components/header/Navigation.vue
+++ components/header/Navigation.vue
@@ -19,7 +19,7 @@
               <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                 <MagnifyingGlassIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
               </div>
-              <input id="search" name="search" class="block w-full rounded-md border-0 bg-dark pl-10 pr-3 text-gray-50 ring-1 ring-inset ring-gray-300 placeholder:text-gray-100 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6" placeholder="Search events..." type="search" />
+              <input id="search" name="search" class="block w-full rounded-md border-0 dark pl-10 pr-3 text-gray-50 ring-1 ring-inset ring-gray-300 placeholder:text-gray-100 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6" placeholder="Search events..." type="search" />
             </div>
           </div>
         </div>
